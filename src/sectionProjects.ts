ce925ec2266d655debce88bbd9556567e076f9f1
Thiago <thiiagooac7@gmail.com>
2024-12-20 14:36:15 -0300

    feat: adding tcc

--- src/sectionProjects.ts
+++ src/sectionProjects.ts
@@ -6,62 +6,69 @@ function sectionProjects(element: HTMLDivElement) {
     const projects = [
 
         {
-            "title" : "ML Application: Data Ingestion, Quality Control, and Prediction",
-            "subtitle" : "A ML Powered application with focus on the operational side. Airflow for automated Data Ingestions, Data Cleaning and Predictions",
-            "technologies" : "Data Science, FastAPI, Streamlit, PostgreSQL, Airflow",
-            "github" : "https://github.com/radtklau/dsp23-project/tree/main"
+            "title": "Identifying and Analyzing Communities in YouTube Comment Sections",
+            "subtitle": "A research paper that analyzes communities formed in YouTube comment sections of content creators targeting children and teenagers, using data from 10 Brazilian YouTubers. The study employs co-commenter network analysis, community detection algorithms, topic modeling, and sentiment analysis to understand user interactions and community dynamics.",
+            "technologies": "Python, NetworkX, Pandas, Scikit-Learn, Matplotlib, BERTopic, NLTK, Sentiment Analysis",
+            "github": "https://github.com/ThiiagoAC7/crawling_youtube",
         },
 
         {
-            "title" : "NLP Sentiment Analysis",
-            "subtitle" : "sentiment Analysis using a Fully Connected Neural Network and a Recurrent Neural Network",
-            "technologies" : "TensorFlow, Keras, NLTK, Scikit-Learn",
-            "github" : "https://github.com/ThiiagoAC7/nlp-sentiment-analysis"
-        },
-        {
-            "title" : "Data Analysis of Amazon Deforestation Datasets",
-            "subtitle" : "data Analysis of different Amazon Deforestation and Climate datasets",
-            "technologies" : "Plotly, Matplotlib, Pandas, Data Science",
-            "github" : "https://github.com/ThiiagoAC7/data-viz-amazon-deflorestation"
-        },
-        {
-            "title" : "AI Image Generated Dataset",
-            "subtitle" : "creation of a Raccoon Dataset using AI-generated images, then used to retrain and compare YOLO v8 models",
-            "technologies" : "Stable Diffusion, Image Generation, Text Generation, YOLO v8, Computer Vision",
-            "github" : "https://github.com/ThiiagoAC7/comp-viz-study/tree/master/final-project"
-        },
-        {
-            "title" : "NeoVim Config",
-            "subtitle" : "my current neovim config",
-            "technologies" : "neovim, lua",
-            "github" : "https://github.com/thiiagoac7/nvim"
-        },
-        {
-            "title" : "Linux Configuration Files (.dotfiles)",
-            "subtitle" : "my current .dotfiles ",
-            "technologies" : "linux, i3-wm, polybar, zsh, wezterm, rofi",
-            "github" : "https://github.com/thiiagoac7/.config"
+            "title": "ML Application: Data Ingestion, Quality Control, and Prediction",
+            "subtitle": "A ML Powered application with focus on the operational side. Airflow for automated Data Ingestions, Data Cleaning and Predictions",
+            "technologies": "Data Science, FastAPI, Streamlit, PostgreSQL, Airflow",
+            "github": "https://github.com/radtklau/dsp23-project/tree/main"
         },
 
         {
-            "title" : "Computer Graphics Algorithms",
-            "subtitle" : "Simple TKInter GUI and Python implementation of computer graphics algorithms (2D transformations, DDA and Bresenham Line Drawing, Circle drawing Bresenham, Cohen-Sutherland and Liang-Barsky line Clipping)",
-            "technologies" : "Python, TKInter",
-            "github" : "https://github.com/ThiiagoAC7/tp1-comp-grafica"
+            "title": "NLP Sentiment Analysis",
+            "subtitle": "sentiment Analysis using a Fully Connected Neural Network and a Recurrent Neural Network",
+            "technologies": "TensorFlow, Keras, NLTK, Scikit-Learn",
+            "github": "https://github.com/ThiiagoAC7/nlp-sentiment-analysis"
+        },
+        {
+            "title": "Data Analysis of Amazon Deforestation Datasets",
+            "subtitle": "data Analysis of different Amazon Deforestation and Climate datasets",
+            "technologies": "Plotly, Matplotlib, Pandas, Data Science",
+            "github": "https://github.com/ThiiagoAC7/data-viz-amazon-deflorestation"
+        },
+        {
+            "title": "AI Image Generated Dataset",
+            "subtitle": "creation of a Raccoon Dataset using AI-generated images, then used to retrain and compare YOLO v8 models",
+            "technologies": "Stable Diffusion, Image Generation, Text Generation, YOLO v8, Computer Vision",
+            "github": "https://github.com/ThiiagoAC7/comp-viz-study/tree/master/final-project"
+        },
+        {
+            "title": "NeoVim Config",
+            "subtitle": "my current neovim config",
+            "technologies": "neovim, lua",
+            "github": "https://github.com/thiiagoac7/nvim"
+        },
+        {
+            "title": "Linux Configuration Files (.dotfiles)",
+            "subtitle": "my current .dotfiles ",
+            "technologies": "linux, i3-wm, polybar, zsh, wezterm, rofi",
+            "github": "https://github.com/thiiagoac7/.config"
         },
 
         {
-            "title" : "Deterministic Finite Automaton Minimization Algorithm",
-            "subtitle" : "Implementation of the O(n^2) and O(n log n) DFA minimization algorithms in Python, following Blum(1996) and Hopcroft(1971)",
-            "technologies" : "Python, JFlap, LaTex ",
-            "github" : "https://github.com/ThiiagoAC7/ftc-minimizing-afd"
+            "title": "Computer Graphics Algorithms",
+            "subtitle": "Simple TKInter GUI and Python implementation of computer graphics algorithms (2D transformations, DDA and Bresenham Line Drawing, Circle drawing Bresenham, Cohen-Sutherland and Liang-Barsky line Clipping)",
+            "technologies": "Python, TKInter",
+            "github": "https://github.com/ThiiagoAC7/tp1-comp-grafica"
         },
 
         {
-            "title" : "Leetcoding",
-            "subtitle" : "Leetcode exercises - learning new languages and practicing algorithms",
+            "title": "Deterministic Finite Automaton Minimization Algorithm",
+            "subtitle": "Implementation of the O(n^2) and O(n log n) DFA minimization algorithms in Python, following Blum(1996) and Hopcroft(1971)",
+            "technologies": "Python, JFlap, LaTex ",
+            "github": "https://github.com/ThiiagoAC7/ftc-minimizing-afd"
+        },
+
+        {
+            "title": "Leetcoding",
+            "subtitle": "Leetcode exercises - learning new languages and practicing algorithms",
             "technologies": "C, C++, Rust",
-            "github" : "https://github.com/thiiagoac7/leetcoding"
+            "github": "https://github.com/thiiagoac7/leetcoding"
         },
 
     ]
